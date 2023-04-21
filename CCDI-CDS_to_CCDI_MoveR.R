76ec0ced5dbb8493984c7f08a6c354ed94f22316
svburke <sean.burke2@nih.gov>
2023-04-21 14:45:20 -0500

    Update CCDI-CDS_to_CCDI_MoveR.R

--- CCDI-CDS_to_CCDI_MoveR.R
+++ CCDI-CDS_to_CCDI_MoveR.R
@@ -22,7 +22,7 @@
 ##################
 
 #List of needed packages
-list_of_packages=c("readr","dplyr","tidyr","knitr","openxlsx","stringi","readxl","janitor","optparse","tools")
+list_of_packages=c("readr","dplyr","openxlsx","stringi","readxl","janitor","optparse","tools")
 
 #Based on the packages that are present, install ones that are required.
 new.packages <- list_of_packages[!(list_of_packages %in% installed.packages()[,"Package"])]
@@ -31,8 +31,6 @@ suppressMessages(if(length(new.packages)) install.packages(new.packages, repos =
 #Load libraries.
 suppressMessages(library(readr,verbose = F))
 suppressMessages(library(dplyr,verbose = F))
-suppressMessages(library(tidyr,verbose = F))
-suppressMessages(library(knitr,verbose = F))
 suppressMessages(library(readxl,verbose = F))
 suppressMessages(library(openxlsx, verbose = F))
 suppressMessages(library(stringi,verbose = F))
